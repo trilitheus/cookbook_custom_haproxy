20fa720c94537fc184280bfc19e6b67bd0ee790e
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-08-25 14:29:48 +0100

    Add comments to client.rb

--- client.rb
+++ client.rb
@@ -1,4 +1,5 @@
 # This file is only used when running chef in local mode during image creation
+# cd /home/vagrant/cookbooks/custom_haproxyi && sudo chef-client -z -c client.rb -o custom_haproxy
 cookbook_path '/home/vagrant/cookbooks'
 data_bag_path '/home/vagrant/cookbooks/custom_haproxy/data_bags'
 environment_path '/home/vagrant/cookbooks/custom_haproxy/environments'
