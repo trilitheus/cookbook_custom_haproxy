0391acf032a2b730ec4198f66fdefda76d2afd44
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-08-26 15:42:15 +0100

    Amend cookbook path

--- client.rb
+++ client.rb
@@ -1,8 +1,8 @@
 # This file is only used when running chef in local mode during image creation
-# cd /home/vagrant/cookbooks/custom_haproxyi && sudo chef-client -z -c client.rb -o custom_haproxy
-cookbook_path '/home/vagrant/cookbooks'
-data_bag_path '/home/vagrant/cookbooks/custom_haproxy/data_bags'
-environment_path '/home/vagrant/cookbooks/custom_haproxy/environments'
+# cd /home/vagrant/cookbooks/custom_haproxy && sudo chef-client -z -c client.rb -o custom_haproxy
+cookbook_path '/home/vagrant'
+data_bag_path '/home/vagrant/custom_haproxy/data_bags'
+environment_path '/home/vagrant/custom_haproxy/environments'
 environment 'kitchen'
 local_mode 'true'
 log_level :info
